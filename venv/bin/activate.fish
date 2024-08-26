042b302a46353ae597aaf94b6e7995873f48766b
lilcoded7 <lilcoded7@gmail.com>
2024-08-26 18:00:12 +0100

    updated

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/coded/Desktop/chatbotbackend/venv"
+set -gx VIRTUAL_ENV "/home/coded/Desktop/Chat-Bot-Backend-Mental-Health/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
